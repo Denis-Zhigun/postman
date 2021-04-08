2e4136d0b5413239dc603819a58263b64667485f
Denis Zhigun <zhigundv@gmail.com>
2021-04-08 22:32:58 +0300

    testing postman

---
+++ src/test/java/PostmanTest.java
@@ -0,0 +1,31 @@
+import io.restassured.http.ContentType;
+import org.junit.Test;
+
+import static io.restassured.RestAssured.given;
+import static org.hamcrest.CoreMatchers.equalTo;
+
+public class PostmanTest {
+
+
+    @Test
+    public void postmanTestPostRequest(){
+
+        // Given - When - Then
+// Предусловия
+        given()
+                .baseUri("https://postman-echo.com")
+                .body("some data") // отправляемые данные (заголовки и query можно выставлять аналогично)
+// Выполняемые действия
+                .when()
+                .post("/post")
+// Проверки
+                .then()
+                .statusCode(200)
+                .contentType(ContentType.JSON)
+                .body("data", equalTo("some data"))
+        ;
+
+
+    }
+
+}
