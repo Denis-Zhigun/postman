84f44852a4111d5abc6feb41cefe4e05d6088db1
Denis Zhigun <zhigundv@gmail.com>
2021-04-08 22:46:00 +0300

    corrected tests data

--- src/test/java/PostmanTest.java
+++ src/test/java/PostmanTest.java
@@ -1,5 +1,5 @@
 import io.restassured.http.ContentType;
-import org.junit.Test;
+import org.junit.jupiter.api.Test;
 
 import static io.restassured.RestAssured.given;
 import static org.hamcrest.CoreMatchers.equalTo;
